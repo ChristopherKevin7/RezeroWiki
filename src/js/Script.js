ac567720406b96b6e7f634e553f1702452ff8435
Christopher Kevin Costa <113649691+ChristopherKevin7@users.noreply.github.com>
2023-12-04 20:16:16 -0300

    Atualizações no site.
    Adicionado uma barra de rolagem na pagina de personagens, além de algumas modificações na pagina inicial.

--- src/js/Script.js
+++ src/js/Script.js
@@ -2,21 +2,22 @@ const botoes = document.querySelectorAll(".botao");
 const personagens = document.querySelectorAll(".personagem");
 
 botoes.forEach((botao, indice) => {
-	botao.addEventListener("click", () => {
-		desselecionarBotao();
-		desselecionarPersonagem();
+    botao.addEventListener("click", () => {
+        desselecionarBotao();
+        desselecionarPersonagem();
 
-		botao.classList.add("selecionado");
-		personagens[indice].classList.add("selecionado");
-	});
+        botao.classList.add("selecionado");
+        personagens[indice].classList.add("selecionado");
+
+    });
 });
 
 function desselecionarPersonagem() {
-	const personagemSelecionado = document.querySelector(".personagem.selecionado");
-	personagemSelecionado.classList.remove("selecionado");
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+    personagemSelecionado.classList.remove("selecionado");
 }
 
 function desselecionarBotao() {
-	const botaoSelecionado = document.querySelector(".botao.selecionado");
-	botaoSelecionado.classList.remove("selecionado");
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
 }
